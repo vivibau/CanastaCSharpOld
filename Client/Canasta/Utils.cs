073057c20ab76b1fe973f4029046aaa1f76825e1
vivibau <vivi16bau@gmail.com>
2015-03-26 15:31:13 +0100

    Added support for reading from formatted strings.

---
+++ Client/Canasta/Utils.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Canasta
+{
+    class Utils
+    {
+        public int readNextInt(string source, ref int pos)
+        {
+            int result;
+            result = source[pos];
+            pos++;
+            return result;
+        }
+
+        public string readNextString(string source, ref int pos)
+        {
+            string result = "";
+            int originalPos = pos;
+            pos++;
+            for (; pos < (char)source[originalPos]; pos++)
+                result += (char)source[pos];
+            return result;
+        }
+
+        public string readString(string source)
+        {
+            string result = "";
+            int originalPos = 0;
+            for (int i = 0; i < (char)source[originalPos]; i++)
+                result += (char)source[i];
+            return result;
+        }
+    }
+}
