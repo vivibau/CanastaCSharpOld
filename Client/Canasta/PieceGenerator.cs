578efc16c53605b3338eee05bbc17b74777ac82e
vivibau <vivi16bau@gmail.com>
2015-03-30 11:56:04 +0200

    fix random piece generator

--- Client/Canasta/PieceGenerator.cs
+++ Client/Canasta/PieceGenerator.cs
@@ -13,16 +13,27 @@ namespace Canasta
         {
             m_pieces = new List<int>();
             List<int> x = new List<int>();
-            for (int i = 0; i < 106; i++)
+            for (int i = 0; i < 107; i++)
                 x.Add(i + 8);
 
             Random rnd = new Random();
-
-            for (int i = 0; i < 106; i++)
+            for (int i = 0; i < 107; i++)
             {
                 int r = rnd.Next(x.Count() - 1);
                 m_pieces.Add(x[r]);
                 x.Remove(x[r]);
+            }
+
+            // for some reason the last piece is 106 + 8 = 113
+            // but the rest are random
+            // so make the series one element bigger and remove the last element (now 114)
+            m_pieces.Remove(m_pieces[m_pieces.Count() - 1]);
+            using (System.IO.StreamWriter file = new System.IO.StreamWriter(@"log.txt"))
+            {
+                for (int i = 0; i < m_pieces.Count(); i++)
+                {
+                    file.WriteLine(m_pieces[i] + " ");
+                }
             }
         }
 
